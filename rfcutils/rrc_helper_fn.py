8ece0614bd990339a7ae014e722236bc4da0248e
TheCatakist <arielrod100@gmail.com>
2024-11-18 21:33:43 +0200

    added comments
    added comments on rcc_helper_fn.py and sig_utils_fn.py

--- rfcutils/rrc_helper_fn.py
+++ rfcutils/rrc_helper_fn.py
@@ -1,17 +1,30 @@
+#  https://nvlabs.github.io/sionna/examples/Pulse_shaping_basics.html#Pulse-shaping-of-a-sequence-of-QAM-symbols
+
 import sionna as sn
-import numpy as np
-import tensorflow as tf
 
 
 def get_psf(samples_per_symbol, span_in_symbols, beta):
-    # samples_per_symbol: Number of samples per symbol, i.e., the oversampling factor
-    # beta: Roll-off factor
-    # span_in_symbols: Filter span in symbold
+    """
+    samples_per_symbol: Number of samples per symbol, i.e., the oversampling factor
+    span_in_symbols: Filter span in symbols
+    beta: Roll-off factor
+
+    returns pulse-shaped-filter (root-raised-cosine) with the specified parameters
+    """
     rrcf = sn.signal.RootRaisedCosineFilter(span_in_symbols, samples_per_symbol, beta)
     return rrcf
 
 
 def matched_filter(sig, samples_per_symbol, span_in_symbols, beta):
+    """
+    sig: input signal
+    samples_per_symbol: Number of samples per symbol, i.e., the oversampling factor
+    span_in_symbols: Filter span in symbols
+    beta: Roll-off factor
+
+    Applies RRC matched-filter on input signal
+    """
     rrcf = get_psf(samples_per_symbol, span_in_symbols, beta)
+    # apply the matched filter
     x_mf = rrcf(sig, padding="same")
     return x_mf
--- rfcutils/sig_utils_fn.py
+++ rfcutils/sig_utils_fn.py
@@ -1,9 +1,16 @@
 import numpy as np
 
+# Pow = (1/len(s))*|s|^2
 get_pow = lambda s: np.mean(np.abs(s) ** 2)
 
 
 def get_sinr(s, i, units='dB'):
+    """
+    s: signal
+    i: interference
+
+    returns the SNIR in dB unless specified otherwise
+    """
     sinr = get_pow(s) / get_pow(i)
     if units == 'dB':
         return 10 * np.log10(sinr)
